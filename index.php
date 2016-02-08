8f4aa66b549eb6ef539c5e8054bcf9bc37686992
James Green <jamesgreen@mail.usf.edu>
2016-02-07 20:47:21 -0500

    Added cute turtle to home page

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<h1>Hello World</h1>
+<div style="margin: 0 auto; text-align: center;">
+	<h1>Hello World</h1>
+
+	<img src="img/sea-turtle.jpg" alt="Sea Turtle" style="width: 75%">
+
+	<h2>Welcome to my site</h2>
+</div>
