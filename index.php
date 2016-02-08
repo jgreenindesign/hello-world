486c5e1831a47aa66792ef533e8cdfd4251214d9
James Green <jamesgreen@mail.usf.edu>
2016-02-07 20:53:02 -0500

    Minor tweaks to the image and php file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <div style="margin: 0 auto; text-align: center;">
 	<h1>Hello World</h1>
 
-	<img src="img/sea-turtle.jpg" alt="Sea Turtle" style="width: 75%">
+	<img src="img/sea-turtle.jpg" alt="Sea Turtle">
 
 	<h2>Welcome to my site</h2>
 </div>
